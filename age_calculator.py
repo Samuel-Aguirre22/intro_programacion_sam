cc2c2b64f68a2bea90d52a1205f9ceb6cf7428bc
jpenuela420 <jpenuela560@soyudemedellin.edu.co>
2024-08-21 09:06:35 -0500

    the generations were added

---
+++ age_calculator.py
@@ -0,0 +1,27 @@
+nombre = input ("¿cual es tu nombre?: ")
+
+ano_n = int(input (("¿en que año naciste?: ")))
+
+edad =int(2024-ano_n)
+
+print (("hola"),(nombre),("tienes o vas a cumplir"),(edad) )
+
+
+if (ano_n) <=1948:
+    print ("y perteneces a los","Niños de la post guerra")
+    
+elif ano_n <= 1968:
+    print("y perteneces a los","Baby boomer")
+    
+elif ano_n <= 1980:
+    print("y perteneces a la","Generacion x")
+    
+elif ano_n <= 1993:
+    print("y perteneces a los","Milenials")
+    
+elif ano_n <= 2010:
+    print("y perteneces a la","Generacion z")
+    
+elif ano_n <= 2024:
+    print("y perteneces a los","Alfa")
+    
